02ff6663875c42ffcb0a233742196729ca540e5c
sopheakneath <sopheakneathphan@gmail.com>
2026-03-04 10:32:12 +0700

    Initial Commit

---
+++ LHPPWallet/LHPPWalletApp.swift
@@ -0,0 +1,21 @@
+//
+//  LHPPWalletApp.swift
+//  LHPPWallet
+//
+//  Created by sopheakneath on 4/3/26.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct LHPPWalletApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
