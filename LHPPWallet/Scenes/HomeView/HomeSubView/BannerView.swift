a817b406d6455a1a699842a65cd8f6a821ef0985
sopheakneath <sopheakneathphan@gmail.com>
2026-04-02 10:08:54 +0700

    home feature

--- LHPPWallet/Scenes/HomeView/HomeSubView/BannerView.swift
+++ LHPPWallet/Scenes/HomeView/HomeSubView/BannerView.swift
@@ -5,11 +5,56 @@
 //  Created by sopheakneath on 1/4/26.
 //
 
+
+
+//
+
 import SwiftUI
+import Combine
+//import Combine
 
 struct BannerView: View {
+    let images = ["logo", "aba", "wing"]
+
+    @State private var currentIndex = 0
+
+    // Timer for auto scroll
+    let timer = Timer.publish(every: 3, on: .main, in: .common).autoconnect()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView(selection: $currentIndex) {
+            ForEach(0..<images.count, id: \.self) { index in
+                VStack {
+                    Image(images[index])
+                        
+                        .resizable()
+                        .scaledToFill()
+                        .cornerRadius(30)
+                       // .frame(maxWidth: 300)
+                        .tag(index)
+                        .clipped()
+                        .padding(20)
+                        .foregroundColor(Color.success)
+                        //.background(Color.red)
+                        .cornerRadius(30)
+                }
+                .cornerRadius(30)
+                .background(Color.vividBlue)
+                .background(Color.prettyGreen)
+                    
+            }
+        }
+      
+        //.background(Color.blue)
+        .frame(height: 200)
+        .frame(width: 300)
+       
+        .tabViewStyle(PageTabViewStyle())
+        .onReceive(timer) { _ in
+            withAnimation {
+                currentIndex = (currentIndex + 1) % images.count
+            }
+        }
     }
 }
 
